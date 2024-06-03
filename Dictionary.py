4bd596bd8fd85c1720f8c781bd0b83726ec5fbf6
GaneshSawatee <163160413+GaneshSawatee@users.noreply.github.com>
2024-06-03 17:26:49 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,27 @@
+phoneBook = {
+    "Dolly" : "1234567890",
+    "Anu" : "9876543210",
+    "Avinash" : "21323422222",
+    "Pooja" : "878975457"
+}
+print(phoneBook)
+
+print(phoneBook["Pooja"])
+
+
+print(phoneBook["Anu"])
+
+
+courses ={
+    "C" :"499",
+    "C++" :"499",
+    "Python" :"499",
+    "DSA" :"999",
+    "ICP" :"999",
+    "ICGP" :"10,000"
+}
+print(courses.keys())
+
+print(courses.values())
+
+print(courses)
